0cb31ae22968ea3e11517da20cb0d100e7e63891
Magnus <Magnus.bryntheson@hotmail.com>
2018-04-24 13:40:23 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,26 @@
-function initMap(){var map=new google.maps.Map(document.getElementById('map'),{center:{lat:58.348732,lng:12.330591},zoom:12});var marker=new google.maps.Marker({position:{lat:58.348732,lng:12.330591},map:map})
-infoWindow=new google.maps.InfoWindow;if(navigator.geolocation){navigator.geolocation.getCurretPosition(function(position){var pos={lat:position.coords.latitude,lng:position.coords.longitude};map.setCenter(pos);calcRoute(pos)},function(){handleLocationError(!0,infoWindow,map.getCenter())},{timeout:10000})}else{handleLocationError(!1,infoWindow,map.getCenter())}
-function handleLocationError(browserHasGeolocation,infoWindow,pos){infoWindow.setPosition(pos);infoWindow.setContent(browserHasGeolocation?'Error: The Geolocation service failed.':'Error: Your browser doesn\'t support geolocation.');infoWindow.open(map)}
-var directionsDisplay=new google.maps.DirectionsRenderer();var directionsService=new google.maps.DirectionsService();directionsDisplay.setMap(map);function calcRoute(pos){var start=pos;var end={lat:58.348732,lng:12.330591};var request={origin:start,destination:end,travelMode:'DRIVING'};directionsService.route(request,function(result,status){if(status=='OK'){directionsDisplay.setDirections(result)}})}}
-function myFunction(){var x=document.getElementById("myTopnav");if(x.className==="topnav"){x.className+=" responsive"}else{x.className="topnav"}}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(i) {
+  showSlides(slideIndex += i);
+}
+
+function currentSlide(i) {
+  showSlides(slideIndex = i);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
